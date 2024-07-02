819ae8d4f56a503542d8779d890b3d8cc63614f4
Miguel Gebhardt <miguel.gebhardt@bechtle.com>
2024-07-02 14:26:34 +0200

    feat: :sparkles: init exercises

---
+++ algorithms/binarySearch.js
@@ -0,0 +1,3 @@
+
+// Create a function that takes a sorted array and a target value. 
+// Return the index of the target value in the array. If the target value is not in the array, return -1.
---
+++ algorithms/lru.js
@@ -0,0 +1,5 @@
+
+//Create a data structure that implements the requirements of a Least Recently Used (LRU) cache with O(1) average time complexity.
+// - Initialize an object with a maxium capacity of elements.
+// - getItem Return the value of the key. Update cache with the most recently used key.
+// - putItem Create or update a key value pair in the cache. Evict the least recently used key if the size of keys exceeds the max capacity.
